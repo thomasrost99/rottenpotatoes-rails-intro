be66fec1e2cdc00f75375888fb584ca12ffe72cf
Ubuntu <ubuntu@ip-172-31-0-102.us-east-2.compute.internal>
2020-09-15 04:10:56 +0000

    all parts complete. oops didn't commit before

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,18 +5,15 @@ class MoviesController < ApplicationController
   end
 
   def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
+    id = params[:id]
+    @movie = Movie.find(id)
   end
 
   def index
-    # for checkbox form on ratings
     @all_ratings = Movie.all_ratings
     
     @movies = Movie.all
 
-    # save our settings in the session store
     session[:settings] = {} unless session[:settings]
     if params[:key]
       params[:ratings] ||= session[:settings]['ratings']
@@ -29,18 +26,13 @@ class MoviesController < ApplicationController
       session[:settings]['ratings'] = params[:ratings]
     end
 
-    # switches for individual ordering based on our parameters
     if params[:key] == 'title'
-      # @title_glyph = glyph_html
       @title_css = 'hilite'
       @movies = Movie.order(:title)
     elsif params[:key] == 'release_date'
-      # @release_glyph = glyph_html
       @release_css = 'hilite'
       @movies = Movie.order(:release_date)
-    # if there were saved settings, redirect to that
     else
-      # restructure this catch case of no params elements?
       if session[:settings]
         flash.keep
         if session[:settings]['key']
@@ -51,7 +43,6 @@ class MoviesController < ApplicationController
           return
         end
       end
-      # no session store catch-all case
       @movies = Movie.all
     end
     if session[:settings] && (not params[:ratings])
@@ -69,7 +60,6 @@ class MoviesController < ApplicationController
   end
 
   def new
-    # default: render 'new' template
   end
 
   def create
